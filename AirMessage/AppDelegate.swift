3367d24c926c60c63551251e5ec316bb70673f4e
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-01-03 08:50:44 -0500

    Initial Commit

---
+++ AirMessage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AirMessage
+//
+//  Created by Cole Feuer on 2021-01-03.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ AirMessage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AirMessage
+//
+//  Created by Cole Feuer on 2021-01-03.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
