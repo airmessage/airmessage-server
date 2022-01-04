2c16eb0ce66746119ef0bec21d6bce9cc46367c0
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-01-04 15:52:33 -0500

    Fix SQL query for specific chat details

--- AirMessage/Database/SQL/QuerySpecificChatDetails.sql
+++ AirMessage/Database/SQL/QuerySpecificChatDetails.sql
@@ -3,9 +3,8 @@ SELECT
 	chat.display_name AS "chat.display_name",
 	chat.service_name AS "chat.service_name",
 	GROUP_CONCAT(handle.id) AS member_list
-FROM
-	chat
+FROM chat
 	JOIN chat_handle_join ON chat.ROWID = chat_handle_join.chat_id
 	JOIN handle ON chat_handle_join.handle_id = handle.ROWID
-WHERE
-	chat.guid IN(?)
+WHERE chat.guid IN(?)
+GROUP BY chat.ROWID
