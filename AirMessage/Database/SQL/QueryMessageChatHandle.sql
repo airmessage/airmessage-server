e16caaec403aa89c3ed0681c0d2496a336f8166f
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-01-05 17:31:48 -0500

    Clean up SQL

--- AirMessage/Database/SQL/QueryMessageChatHandle.sql
+++ AirMessage/Database/SQL/QueryMessageChatHandle.sql
@@ -2,6 +2,6 @@ SELECT %1$@ /* Fields */
 FROM message
     JOIN chat_message_join ON message.ROWID = chat_message_join.message_id
     JOIN chat ON chat_message_join.chat_id = chat.ROWID
-    LEFT OUTER JOIN handle AS sender_handle ON message.handle_id = sender_handle.ROWID
-    LEFT OUTER JOIN handle AS other_handle ON message.other_handle = other_handle.ROWID
+    LEFT JOIN handle AS sender_handle ON message.handle_id = sender_handle.ROWID
+    LEFT JOIN handle AS other_handle ON message.other_handle = other_handle.ROWID
 %2$@ /* Extra query statements */
