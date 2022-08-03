82855df1207b1d2d2a230a2af2eeb00702eccbcb
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-08-03 15:55:42 -0400

    Update QueryOutgoingMessages.sql

--- AirMessage/Database/SQL/QueryOutgoingMessages.sql
+++ AirMessage/Database/SQL/QueryOutgoingMessages.sql
@@ -5,9 +5,10 @@ SELECT max(message.ROWID) as "message.ROWID",
        message.is_read AS "message.is_read",
        message.date_read AS "message.date_read",
        chat.ROWID AS "chat.ROWID"
-FROM message
-    JOIN chat_message_join ON message.ROWID = chat_message_join.message_id
-    JOIN chat ON chat_message_join.chat_id = chat.ROWID
-WHERE message.is_from_me IS 1
+FROM chat
+    LEFT OUTER JOIN chat_message_join ON chat.ROWID = chat_message_join.chat_id
+    LEFT OUTER JOIN message ON chat_message_join.message_id = message.ROWID
+WHERE message.guid IS NOT NULL
+    AND message.is_from_me IS 1
 %1$@ /* Extra query statements */
 GROUP BY chat.ROWID
