3570a642526386fea8e18174f9244a935086f92b
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-10-20 17:42:46 -0400

    Fix QueryEditedMessages.sql

--- AirMessage/Database/SQL/QueryEditedMessages.sql
+++ AirMessage/Database/SQL/QueryEditedMessages.sql
@@ -4,6 +4,8 @@ SELECT message.ROWID as "message.ROWID",
        message.message_summary_info AS "message.message_summary_info",
        chat.ROWID AS "chat.ROWID"
 FROM message
+    LEFT OUTER JOIN chat_message_join ON message.ROWID = chat_message_join.message_id
+    LEFT OUTER JOIN chat ON chat_message_join.chat_id = chat.ROWID
 WHERE message.date_edited > ?
 %1$@ /* Extra query statements */
 GROUP BY chat.ROWID
